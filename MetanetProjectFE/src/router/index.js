81bccd41df10834e35bfe2210459dc83ae0e51d8
chaeeun-Han <chaeeun.h911@gmail.com>
2025-01-31 16:04:58 +0900

    feat: Modify public files (#1)
    Co-authored-by: udongmandu <qjawns10000@naver.com>

---
+++ MetanetProjectFE/src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Signup from "../components/Signup.vue";
+import Account_main from "../components/Account/Account_main.vue";
+
+const routes = [
+  { path: "/", component: Signup },
+  { path: "/account", component: Account_main },
+  { path: "/signup", component: Signup },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
