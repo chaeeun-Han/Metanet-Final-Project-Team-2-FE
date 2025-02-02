769e6c2a1690370505b5385459f39b8f044e3e34
udongmandu <qjawns10000@naver.com>
2025-02-03 00:44:48 +0900

    feat: Add Login router

--- MetanetProjectFE/src/router/index.js
+++ MetanetProjectFE/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Signup from "../components/Signup.vue";
 import Account_main from "../components/Account/Account_main.vue";
+import Login_main from "../components/login/Login_main.vue";
 
 const routes = [
   { path: "/", component: Signup },
   { path: "/account", component: Account_main },
   { path: "/signup", component: Signup },
+  { path: "/login", component: Login_main },
 ];
 
 const router = createRouter({
