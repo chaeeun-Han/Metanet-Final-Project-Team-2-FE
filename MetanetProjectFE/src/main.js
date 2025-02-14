6ebd8547532ccb7df7b2f0a7a8886e63e14cba55
sinyeongseo <jasmine88368@naver.com>
2025-02-14 10:59:27 +0900

    feat : Add login Interceptor (#41)

--- MetanetProjectFE/src/main.js
+++ MetanetProjectFE/src/main.js
@@ -5,34 +5,28 @@ import i18n from './i18n';
 import axios from 'axios';
 
 axios.interceptors.response.use(
-    response => response, 
+    response => response,
     async (error) => {
-        if (error.response) {
-            const status = error.response.status;
-          
-            if (status === 403) {
-                await Swal.fire('오류', '접근 제한이 있는 페이지입니다.', 'error');
-                router.push({ name: 'Error403Page' }).then(() => {                 
-                    window.location.reload();
-                });
-            } else if (status === 400) {
-                await Swal.fire('오류', '잘못된 요청입니다.', 'error');
-                router.push({ name: 'Error400Page' , query: { previousUrl: currentUrl }}).then(() => {                 
-                    window.location.reload();
-                });
-                
-            } else if (status === 500) {                
-                await Swal.fire('오류', '서버에서 예기치 않은 오류가 발생했습니다.', 'error');
-                router.push({ name: 'Error500Page' , query: { previousUrl: currentUrl }}).then(() => {                 
-                    window.location.reload();
-                });                
-            }
-        }
-        return Promise.reject(error);
+      const status = error.response?.status;
+      const skipInterceptor = error.config?.headers?.skipInterceptor;
+        
+      if (skipInterceptor === 'true') return Promise.reject(error);
+  
+      if (status === 403) {
+        await Swal.fire('오류', '접근 제한이 있는 페이지입니다.', 'error');
+        router.push({ name: 'Error403Page' }).then(() => window.location.reload());
+      } else if (status === 400) {
+        await Swal.fire('오류', '잘못된 요청입니다.', 'error');
+        router.push({ name: 'Error400Page' }).then(() => window.location.reload());
+      } else if (status === 500) {
+        await Swal.fire('오류', '서버 오류가 발생했습니다.', 'error');
+        router.push({ name: 'Error500Page' }).then(() => window.location.reload());
+      }
+  
+      return Promise.reject(error);
     }
-);
-
-
+  );
+  
 
 
 const app = createApp(App);
