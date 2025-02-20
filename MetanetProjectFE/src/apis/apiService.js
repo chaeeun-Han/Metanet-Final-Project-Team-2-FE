75adf0b331eb97e569d2c5076ecaad4e3986abfe
lsj90954511 <sojin49@naver.com>
2025-02-20 14:42:35 +0900

    hotfix: Remove unused method

--- MetanetProjectFE/src/apis/apiService.js
+++ MetanetProjectFE/src/apis/apiService.js
@@ -17,27 +17,27 @@ function parseJwt(token) {
  * @param {Object} credentials - { id, password }
  * @returns {Promise} - 응답 데이터
  */
-export async function login(credentials) {
-  const response = await api({
-    method: "post",
-    url: "/auth/login",
-    data: credentials,
-    headers: { "Content-Type": "application/json; charset=UTF-8" },
-  });
+// export async function login(credentials) {
+//   const response = await api({
+//     method: "post",
+//     url: "/auth/login",
+//     data: credentials,
+//     headers: { "Content-Type": "application/json; charset=UTF-8" },
+//   });
 
-  const authHeader = response.headers.authorization;
-  if (authHeader && authHeader.startsWith("Bearer ")) {
-    const token = authHeader.split(" ")[1];
-    sessionStorage.setItem("jwtToken", token);
+//   const authHeader = response.headers.authorization;
+//   if (authHeader && authHeader.startsWith("Bearer ")) {
+//     const token = authHeader.split(" ")[1];
+//     sessionStorage.setItem("jwtToken", token);
 
-    const payload = parseJwt(token);
-    if (payload) {
-      sessionStorage.setItem("memberId", payload.sub);
-      sessionStorage.setItem("memberRole", payload.auth);
-    }
-  }
-  return response.data;
-}
+//     const payload = parseJwt(token);
+//     if (payload) {
+//       sessionStorage.setItem("memberId", payload.sub);
+//       sessionStorage.setItem("memberRole", payload.auth);
+//     }
+//   }
+//   return response.data;
+// }
 
 export function callApi(method, endpoint, data = {}) {
   return api({
